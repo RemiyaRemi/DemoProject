acc255dd4f29009717111f0936c557a1c18d4c1c
root <root@ip-172-31-35-83.ap-south-1.compute.internal>
2026-03-01 14:47:40 +0000

    Code changes for commit3

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 templates = Jinja2Templates(directory = "/code")
 #templates = Jinja2Templates(directory = "D:/balan/DevOps/kubernetes"
-
 @app.get ("/")
 def form_port (request: Request): return templates.TemplateResponse ('form.html', context = {'request': request})
  
